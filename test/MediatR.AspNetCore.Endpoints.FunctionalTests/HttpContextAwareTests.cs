2a44d113e37016608eea4d225d86984ddfddb394
Kahbazi <A.Kahbazi@gmail.com>
2020-04-26 11:07:20 +0430

    Add test for IHttpContextAware

---
+++ test/MediatR.AspNetCore.Endpoints.FunctionalTests/HttpContextAwareTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace MediatR.AspNetCore.Endpoints.FunctionalTests
+{
+    public class HttpContextAwareTests
+    {
+        private TestServer testServer;
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddRouting();
+
+                    services.AddMediatR(GetType().Assembly);
+                    services.AddMediatREndpoints();
+                })
+                .Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapMediatR();
+                    });
+                });
+
+            testServer = new TestServer(builder);
+        }
+
+        [Test]
+        public async Task HttpContextSetsOnRequest()
+        {
+            var client = testServer.CreateClient();
+            await client.GetAsync("/");
+        }
+
+        public class HttpContextAwareRequest : IRequest, IHttpContextAware
+        {
+            public HttpContext HttpContext { get; set; }
+        }
+
+        public class HttpContextAwareRequestHandler : IRequestHandler<HttpContextAwareRequest>
+        {
+            private readonly IHttpContextAccessor _httpContextAccessor;
+
+            public HttpContextAwareRequestHandler(IHttpContextAccessor httpContextAccessor)
+            {
+                _httpContextAccessor = httpContextAccessor;
+            }
+
+
+            public Task<Unit> Handle(HttpContextAwareRequest request, CancellationToken cancellationToken)
+            {
+                Assert.AreSame(_httpContextAccessor.HttpContext, request.HttpContext);
+                return Unit.Task;
+            }
+        }
+    }
+}
