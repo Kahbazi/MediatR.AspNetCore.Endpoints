4e6ef834f197a4400be2848d17698b3319517e98
Kahbazi <A.Kahbazi@gmail.com>
2020-12-07 11:47:43 +0330

    Remove unused property

--- src/MediatR.AspNetCore.Endpoints/MediatorEndpointOptions.cs
+++ src/MediatR.AspNetCore.Endpoints/MediatorEndpointOptions.cs
@@ -5,6 +5,5 @@ namespace MediatR.AspNetCore.Endpoints
     public class MediatorEndpointOptions
     {
         public JsonSerializerOptions JsonSerializerOptions { get; set; }
-        public bool MyProperty { get; set; }
     }
 }
